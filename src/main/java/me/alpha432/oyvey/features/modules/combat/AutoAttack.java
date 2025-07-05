18ddb957a0f8c78895e2e246a244cd274c182a56
Alextheplayer919 <robetsimko@gmail.com>
2025-07-05 16:06:53 +0200

    Update AutoAttack.java

--- src/main/java/me/alpha432/oyvey/features/modules/combat/AutoAttack.java
+++ src/main/java/me/alpha432/oyvey/features/modules/combat/AutoAttack.java
@@ -1,7 +1,7 @@
 package me.alpha432.oyvey.features.modules.combat;
 
 import me.alpha432.oyvey.features.modules.Module;
-import me.alpha432.oyvey.features.setting.Setting; // fixed import
+import me.alpha432.oyvey.features.setting.Setting; 
 import me.alpha432.oyvey.util.models.Timer;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.entity.Entity;
@@ -10,16 +10,16 @@ import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.util.Hand;
 
 import java.util.Comparator;
-import java.util.stream.StreamSupport; // <--- add this
+import java.util.stream.StreamSupport;
 
 public class AutoAttack extends Module {
     private final Timer attackTimer = new Timer();
 
-    // Settings
     public Setting<Float> range = register(new Setting<>("Range", 4.5f, 1.0f, 6.0f));
     public Setting<Integer> delay = register(new Setting<>("Delay", 500, 0, 1000));
     public Setting<Boolean> playersOnly = register(new Setting<>("PlayersOnly", true));
-    public Setting<Boolean> rotate = register(new Setting<>("Rotate", false)); // placeholder
+    public Setting<Boolean> rotate = register(new Setting<>("Rotate", false));
+    public Setting<Boolean> silentRotate = register(new Setting<>("SilentRotate", false));
 
     public AutoAttack() {
         super("AutoAttack", "Automatically attacks players", Category.COMBAT, true, false, false);
@@ -45,8 +45,34 @@ public class AutoAttack extends Module {
     }
 
     private void attack(Entity target) {
-        mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(target, mc.player.isSneaking()));
+        if (silentRotate.getValue()) {
+            float[] rotations = calculateRotations(target);
+            float yaw = rotations[0];
+            float pitch = rotations[1];
+            mc.player.networkHandler.sendPacket(
+                new PlayerInteractEntityC2SPacket.AttackWithRotation(target, yaw, pitch, mc.player.isSneaking())
+            );
+        } else if (rotate.getValue()) {
+            float[] rotations = calculateRotations(target);
+            mc.player.yaw = rotations[0];
+            mc.player.pitch = rotations[1];
+            mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(target, mc.player.isSneaking()));
+        } else {
+            mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(target, mc.player.isSneaking()));
+        }
         mc.player.swingHand(Hand.MAIN_HAND);
+    }
+
+    private float[] calculateRotations(Entity target) {
+        double diffX = target.getX() - mc.player.getX();
+        double diffY = (target.getY() + target.getEyeHeight()) - (mc.player.getY() + mc.player.getEyeHeight());
+        double diffZ = target.getZ() - mc.player.getZ();
+        double dist = Math.sqrt(diffX * diffX + diffZ * diffZ);
+
+        float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90F;
+        float pitch = (float) -Math.toDegrees(Math.atan2(diffY, dist));
+
+        return new float[] {yaw, pitch};
     }
 
     @Override
